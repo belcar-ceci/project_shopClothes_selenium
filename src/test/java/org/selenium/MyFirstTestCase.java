ce7edc0aab2a7b5ce9b4856ef8fb1f8f9817a41d
Cecilia Carbajal <cecilia.carbajal@sportium.com>
2023-06-29 17:38:38 +0200

    refactoring Page Object Model Design First Test

--- src/test/java/org/selenium/MyFirstTestCase.java
+++ src/test/java/org/selenium/MyFirstTestCase.java
@@ -3,43 +3,36 @@ package org.selenium;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.selenium.pom.base.BaseTest;
+import org.selenium.pom.pages.CartPage;
+import org.selenium.pom.pages.CheckoutPage;
+import org.selenium.pom.pages.HomePage;
+import org.selenium.pom.pages.StorePage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class MyFirstTestCase {
+public class MyFirstTestCase extends BaseTest {
 
     @Test
     public void guestCheckoutUsingDirectBankTransfer() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
         driver.get("https://www.askomdch.com/");
-        driver.manage().window().maximize();
-        driver.findElement(By.cssSelector("#menu-item-1227 > a")).click();
-        driver.findElement(By.id("woocommerce-product-search-field-0")).sendKeys("Blue");
-        driver.findElement(By.cssSelector("button[value='Search']")).click();
-        Assert.assertEquals(
-                driver.findElement(By.cssSelector(".woocommerce-products-header__title.page-title")).getText(),
-                "Search results: “Blue”"
-        );
-        driver.findElement(By.cssSelector("a[aria-label='Add “Blue Shoes” to your cart']")).click();
-        Thread.sleep(5000);
-        driver.findElement(By.cssSelector("a[title='View cart']")).click();
-        Assert.assertEquals(
-                driver.findElement(By.cssSelector("td[class='product-name'] a")).getText(),
-                "Blue Shoes"
-        );
-        driver.findElement(By.cssSelector(".checkout-button")).click();
-        driver.findElement(By.id("billing_first_name")).sendKeys("demo");
-        driver.findElement(By.id("billing_last_name")).sendKeys("user");
-        driver.findElement(By.id("billing_address_1")).sendKeys("San Francisco");
-        driver.findElement(By.id("billing_city")).sendKeys("San Francisco");
-        driver.findElement(By.id("billing_postcode")).sendKeys("94188");
-        driver.findElement(By.id("billing_email")).sendKeys("askomdch@gmail.com");
-        driver.findElement(By.id("place_order")).click();
-        Thread.sleep(5000);
-        Assert.assertEquals(driver.findElement(By.cssSelector(".woocommerce-notice")).getText(),
-                "Thank you. Your order has been received.");
 
-        driver.quit();
+        HomePage homePage = new HomePage(driver);
+        StorePage storePage = homePage.navigateToStoreUsingMenu();
+        storePage.search("Blue");
+        Assert.assertEquals(storePage.getTitle(), "Search results: “Blue”");
+        storePage.clickAddToCartBtn("Blue Shoes");
+        Thread.sleep(5000);
+        CartPage cartPage = storePage.clickViewCart();
+
+        Assert.assertEquals(cartPage.getProductName(), "Blue Shoes");
+
+        CheckoutPage checkoutPage = cartPage.checkout();
+        checkoutPage.enterFirstName("demo").enterLastName("user").enterAddressLineOne("San Francisco").enterCity("San Francisco").enterPostCode("94188").enterEmail("askomdch@gmail.com").placeOrder();
+        Thread.sleep(5000);
+        Assert.assertEquals(checkoutPage.getNotice(), "Thank you. Your order has been received.");
+
+
     }
 
     @Test
---
+++ src/test/java/org/selenium/pom/factory/DriverManager.java
@@ -0,0 +1,16 @@
+package org.selenium.pom.factory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverManager {
+
+    public WebDriver initializeDriver(){
+
+        WebDriver driver = new ChromeDriver();
+        //driver.get("https://www.askomdch.com/");
+        driver.manage().window().maximize();
+        return driver;
+
+    }
+}
